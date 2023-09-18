e28e84098707cba3c7e6acf702de5bef666101fc
ValentinaSukonina <v.trana@icloud.com>
2023-09-18 19:01:39 +0200

    2d array sorting

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,7 +17,7 @@ public class App {
             int[] hours = new int[24]; // array for hours (as index for price input
             String[] timeTable = new String[24]; //array for formatted time
             int[][] pricePerHour = new int[24][2]; //2D array with prices and hours as index
-            float mean4h = 0;
+            int hour;
             int minPrice = 0;
             int positionMin = 0;
             int maxPrice = 0;
@@ -25,11 +25,14 @@ public class App {
             double meanPrice = 0;
             double min4h;
             int mean4Start = 0;
+            float mean4h = 0;
 
-            // fill array for hours and array for timeTable with formatted hours string
+
+            // fill "pricePerHour" array with hours and array for timeTable with formatted hours string
             for (int i = 0; i < hours.length; i++) {
                 hours[i] = i;
                 timeTable[i] = String.format("%02d-%02d", i, i + 1);
+                pricePerHour[i][1] = i;
             }
 
                 String choice;
@@ -47,32 +50,16 @@ public class App {
                     System.out.print(menu);
                     choice = scanner.nextLine();
                     if (choice.equals("1")) {
-                        for (int i = 0; i < price.length; i++) {
+                       for (int i = 0; i < pricePerHour.length; i++) {
                             System.out.print(timeTable[i] + " ");
-                            price[i] = Integer.parseInt(scanner.nextLine());
-                        }
-//                        try {
-//                            price[i] = Integer.parseInt(scanner.nextLine());
-//                        } catch (Exception e) {
-//                            System.out.print("Invalid input. Only whole numbers, please\n");
-//                            i -= 1;
-//                        }
-
-                    // Create a new 2D array that contains both prices and hours
-                    for (int i = 0; i < pricePerHour.length; i++) {
-                        pricePerHour[i][0] = hours[i]; //assign the element of array hour to the 1st column
-                        pricePerHour[i][1] = price[i]; //assign the element of array price to the 2nd column
-                    }
+                            pricePerHour[i][0] = Integer.parseInt(scanner.nextLine());
+                       }
                 }
-
                     if (choice.equals("2")) {
                         for (int i = 0; i < pricePerHour.length; i++) {
                             if (i == 0) {
                                 minPrice = pricePerHour[i][0];
                                 positionMin = i;
-                                minPrice = pricePerHour[i][0];
-                            }
-                            if (pricePerHour[i][0] > maxPrice) {
                                 maxPrice = pricePerHour[i][0];
                                 positionMax = i;
                             }
@@ -80,7 +67,12 @@ public class App {
                                 minPrice = pricePerHour[i][0];
                                 positionMin = i;
                             }
-                            meanPrice = meanPrice + pricePerHour[i][0];
+                            if (pricePerHour[i][0] > maxPrice) {
+                                maxPrice = pricePerHour[i][0];
+                                positionMax = i;
+                            }
+                            //meanPrice = meanPrice + pricePerHour[i][0];
+                            meanPrice += pricePerHour[i][0];
                         }
                         meanPrice = meanPrice / 24;
 
@@ -89,14 +81,14 @@ public class App {
                         System.out.print("Medelpris: " + String.format("%.02f", meanPrice) + " öre/kWh\n");
                     }
                     if (choice.equals("3")) {
-                        // Make a copy of the original array with Price in first column, hours in second*/
+                        // Make a copy of the original 2d array "PricePerHour"
                         int[][] copyPricePerHour = Arrays.copyOf(pricePerHour, pricePerHour.length);
+
                         // Sort the copy of Array by the first column using a lambda expression
-                        Arrays.sort(copyPricePerHour, (int[] a, int[] b) -> Integer.compare(a[0], b[0]));
-                        int hour;
+                        Arrays.sort(copyPricePerHour, (int[] a, int[] b) -> Integer.compare(b[0], a[0]));
                         for (int i = 0; i < copyPricePerHour.length; i++) {
-                            hour = pricePerHour[i][1];
-                            System.out.printf(String.format("%02d-%02d", hour, hour + 1) + " " + copyPricePerHour[i][0] + " öre\n");
+                            hour = copyPricePerHour[i][1];
+                            System.out.print(timeTable[hour] + " " + copyPricePerHour[i][0] + " öre\n");
                         }
                     }
                     if (choice.equals("4")) {
